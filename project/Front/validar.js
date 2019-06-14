f55eaf595fbbd2b3702090d21655669f0cd01bbf
SLadeAll <43075858+SLadeAll@users.noreply.github.com>
2019-06-14 07:42:27 -0500

    Add files via upload

---
+++ project/Front/validar.js
@@ -0,0 +1,8 @@
+function validarRepetir(txt1, txt2) {
+    if (txt1 == txt2) {
+        return true;
+    } else
+        return alert('error');
+}
+var em1 = document.getElementById('email').value;
+var em1 = document.getElementById('email1').value;
---
+++ project/Front/validar.ts
@@ -0,0 +1,9 @@
+function validarRepetir(txt1, txt2){
+    if(txt1==txt2){
+        return true;
+    }
+    else 
+    return alert('error')
+}
+
+var a = document.getElementById('nodo').value;
